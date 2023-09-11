522a679b3b6a313bb764b85cad727f4a6c188d34
Maxim Shurygin <maximys2142@mail.ru>
2023-09-11 12:41:00 +0400

    Initial commit

---
+++ service/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from service.views import *
+
+urlpatterns = [
+    # path('', ProductListView.as_view(), name='product_list'),
+    # path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    # path('contacts/', contacts, name='contacts'),
+]
